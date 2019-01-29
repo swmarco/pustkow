7a86be2db93e5a35d95f35e7f4e3dd1dc6894fd3
Marek <Marek@DELL-E5510-1.FSG2200HNU>
2019-01-29 12:33:59 +0100

    Commit Hello

---
+++ src/pustkow/com/hello/Hello.java
@@ -0,0 +1,22 @@
+package pustkow.com.hello;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author Marek Œwiêtek
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !!!");
+	}
+
+}
